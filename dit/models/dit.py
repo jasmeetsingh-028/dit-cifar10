5c687b10416fcbc3b11d9dcc847276f9430a0407
jasmeetsingh-028 <sjasmeet135@gmail.com>
2026-06-23 21:23:29 +0530

    feat: Implementing DIT (forward method)

--- dit/models/dit.py
+++ dit/models/dit.py
@@ -37,4 +37,76 @@ class DIT(nn.Module):
         nn.init.trunc_normal_(self.pos_embed, std=0.02)  # initialize pos embeddings with mean and 
 
         # conditional embedding for class and timestep
-        self.conditional_embed = clas
+        self.conditional_embed = ConditionEmbedding(
+            d_model = d_model,
+            num_classes = num_classes,
+            dropout_p = class_dropout_p
+        )
+
+        # DIT blocks
+        self.dit_blocks = nn.ModuleList(
+            [
+                DITBlock(
+                    d_model, num_heads, mlp_ratio
+                ) for _ in range(n_blocks)
+            ]
+        )
+
+        # final adaln layer and linear layer
+        self.final_norm = nn.LayerNorm(d_model, elementwise_affine=False)
+        self.final_adaLN = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(d_model, 2 * d_model)  # only scaling and shifting, no gate needed here
+        )
+        nn.init.zeros_(self.final_adaLN[-1].weight)
+        nn.init.zeros_(self.final_adaLN[-1].bias)
+
+        patch_dim = patch_size * patch_size * self.out_channels   # 4 * 4 * 3 = 48
+
+        self.final_linear_proj = nn.Linear(d_model, patch_dim) # to unpatchify 384 -> 48
+
+        # zero initilaization for final layer
+
+        nn.init.zeros_(self.final_proj.weight)
+        nn.init.zeros_(self.final_proj.bias)
+    
+    def unpatchify(self, x):
+        # x shape after dit: (B, seq_len/num_patches = 64, 4*4*3 = 48) where patch_Size/patch height and width = 4 and patch channels = 3
+        B, T, _ = x.shape # T = 64
+        P = self.patch_size  # 4
+        H = W = int(T ** 0.5) # sqrt(64) =  8
+        C =  self.out_channels #3
+
+        x = x.reshape(B, H, W, P, P, C) #(B, 8, 8, 4, 4, 3)
+        x = x.permute(B, C, H*P, W*P) #(B, 3, 32, 32)
+        return x
+    
+    def forward(self, x, t, y):
+
+        # x: (B, 3, 32, 32) noisy image
+        # t: (B,) timesteps
+        # y: (B,) class labels
+
+        x = self.patch_embed(x) + self.pos_embed  # patchify image and add positional embeddings to the image
+        # x shape: (B, seq_len/num patches = 64, d_model = 384)
+        c = self.conditional_embed(t, y)
+        # c shape: (B, d_model = 384)
+
+        for block in self.dit_blocks:
+            x = block(x)
+
+        #x shape: (B, seq_len/num patches = 64, d_model = 384)
+
+        # final adaLN (scale + shift only, no gate)
+        gamma, beta = self.final_adaLN(c).chunk(2, dim=-1)
+        x = x * (1 + gamma.unsqueeze(1)) + beta.unsqueeze(1)
+
+        x = self.final_linear_proj(x)
+
+        # x shape: (B, seq_len/num patches = 64, d_model = 384) -> (B, num_patches/seq_len/T = 64, patch_dim = 48 = 4*4*3)
+        x = self.unpatchify(x)
+
+        return x
+    
+    
+    
